03f2e5e9b79ac0c43d9dd7d6e57755d040fa6e9e
Hoksolinvan <chhunhoksolinvan.prof@gmail.com>
2024-05-19 17:00:16 -0700

    Updated Customer.sql
    Added additional commands for querying, data manipulation and data definition.

--- Customer/Customer.sql
+++ Customer/Customer.sql
@@ -9,6 +9,23 @@ load data local infile '/Users/iamvan/Desktop/CSC370/Customer/customer.csv' into
 #displaying table
 select * from `customers`;
 
+#The manager wants to see the table sorted in descending according to the customer ID and he wants us to change "CustomerID" attribute name to "ID" instead
+select * from `customers` order by `CustomerID` desc;
+
+#The manager is looking for a customer with the highest amount of savings to give them a special offer
+select * from `customers` order by `SavingBalance` desc LIMIT 1;
+
 
 #Query for Customer1. They only want to see their Account Number, checking balance and saving balance
 select `AccountNumber`,`CheckingBalance`,`SavingBalance` from `customers` where `CustomerName`='Customer 1';
+
+#Customer1 didn't liked that his name was "Customer1" therefore he threatened to speak to our manager if we didn't change his name to his real name
+update `customers`
+set CustomerName='poopy'
+where CustomerID='1';
+
+select * from `customers` where `CustomerID`=1;
+
+
+
+
