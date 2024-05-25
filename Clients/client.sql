ee12b8441d741f6fc6d75f522f2910537a2b9781
Hoksolinvan <chhunhoksolinvan.prof@gmail.com>
2024-05-25 12:57:48 -0700

    Revamped the csv, excel sheet, and .csv
    got rid of "savingbalance" attribute and changed "checkingbalance" to "balance" attribute throughout the table and its datasheets

--- Clients/client.sql
+++ Clients/client.sql
@@ -1,6 +1,6 @@
 
 #Table Creation
-create table `Clients`(`Client_ID` int primary key, `AccountNumber` int, `ClientName` varchar(40), `CheckingBalance` varchar(40) default '$0', `SavingBalance` varchar(40) default '$0', `AccountType` enum('None','Unleaded','Premium'), `CreditScore` int default 0);
+create table `Clients`(`Client_ID` int primary key, `AccountNumber` int, `ClientName` varchar(40), `Balance` varchar(40) default '$0', `AccountType` enum('None','Unleaded','Premium'), `CreditScore` int default 0);
 
 #Loading file
 load data local infile '/Users/iamvan/Desktop/CSC370/Clients/client.csv' into table `Clients` fields terminated by ',' enclosed by '"' lines terminated by '\r\n' ignore 1 lines;
@@ -13,11 +13,11 @@ select * from `Clients`;
 select * from `Clients` order by `Client_ID` desc;
 
 #The manager is looking for a customer with the highest amount of savings to give them a special offer
-select * from `Clients` order by `SavingBalance` desc LIMIT 1;
+select * from `Clients` order by `Balance` desc LIMIT 1;
 
 
 #Query for Customer1. They only want to see their Account Number, checking balance and saving balance
-select `AccountNumber`,`CheckingBalance`,`SavingBalance` from `Clients` where `ClientName`='Customer 1';
+select `AccountNumber`,`Balance` from `Clients` where `ClientName`='Customer 1';
 
 #Customer1 didn't liked that his name was "Customer1" therefore he threatened to speak to our manager if we didn't change his name to his real name
 update `Clients`
