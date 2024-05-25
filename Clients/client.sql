4a4eee0123dacebece7281c1d274b408f1445dd2
Hoksolinvan <chhunhoksolinvan.prof@gmail.com>
2024-05-25 12:47:01 -0700

    Updated client.sql
    changed attribute name "creditcardtype" to "AccountType"

--- Clients/client.sql
+++ Clients/client.sql
@@ -1,30 +1,30 @@
 
 #Table Creation
-create table `customers`(`CustomerID` int primary key, `AccountNumber` int, `CustomerName` varchar(40), `CheckingBalance` varchar(40) default '$0', `SavingBalance` varchar(40) default '$0', `CreditCardType` enum('None','Unleaded','Premium'), `CreditScore` int default 0);
+create table `Clients`(`Client_ID` int primary key, `AccountNumber` int, `ClientName` varchar(40), `CheckingBalance` varchar(40) default '$0', `SavingBalance` varchar(40) default '$0', `AccountType` enum('None','Unleaded','Premium'), `CreditScore` int default 0);
 
 #Loading file
-load data local infile '/Users/iamvan/Desktop/CSC370/Customer/customer.csv' into table `customers` fields terminated by ',' enclosed by '"' lines terminated by '\r\n' ignore 1 lines;
+load data local infile '/Users/iamvan/Desktop/CSC370/Clients/client.csv' into table `Clients` fields terminated by ',' enclosed by '"' lines terminated by '\r\n' ignore 1 lines;
 
 
 #displaying table
-select * from `customers`;
+select * from `Clients`;
 
 #The manager wants to see the table sorted in descending according to the customer ID and he wants us to change "CustomerID" attribute name to "ID" instead
-select * from `customers` order by `CustomerID` desc;
+select * from `Clients` order by `Client_ID` desc;
 
 #The manager is looking for a customer with the highest amount of savings to give them a special offer
-select * from `customers` order by `SavingBalance` desc LIMIT 1;
+select * from `Clients` order by `SavingBalance` desc LIMIT 1;
 
 
 #Query for Customer1. They only want to see their Account Number, checking balance and saving balance
-select `AccountNumber`,`CheckingBalance`,`SavingBalance` from `customers` where `CustomerName`='Customer 1';
+select `AccountNumber`,`CheckingBalance`,`SavingBalance` from `Clients` where `ClientName`='Customer 1';
 
 #Customer1 didn't liked that his name was "Customer1" therefore he threatened to speak to our manager if we didn't change his name to his real name
-update `customers`
-set CustomerName='poopy'
-where CustomerID='1';
+update `Clients`
+set ClientName='poopy'
+where Client_ID='1';
 
-select * from `customers` where `CustomerID`=1;
+select * from `Clients` where `Client_ID`=1;
 
 
 
