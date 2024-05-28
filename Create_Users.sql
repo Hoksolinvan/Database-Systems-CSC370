4d1990e41a7caee897d5159cb1376186c53eebc4
Adam Zheng <58533225+AdamZheng2004@users.noreply.github.com>
2024-05-28 10:11:25 -0700

    Add files via upload

--- Create_Users.sql
+++ Create_Users.sql
@@ -1,11 +1,14 @@
+DROP USER IF EXISTS 'Adam_Zheng'@'%';
 DROP USER IF EXISTS 'Hoksolinvan_Chhun'@'%';
 DROP USER IF EXISTS 'Shabab_Ali'@'%';
 DROP USER IF EXISTS 'Samuel_Cheng'@'%';
 
+CREATE USER 'Adam_Zheng'@'%' IDENTIFIED BY 'Adam_Zheng';
 CREATE USER 'Hoksolinvan_Chhun'@'%' IDENTIFIED BY 'Hoksolinvan_Chhun';
 CREATE USER 'Shabab_Ali'@'%' IDENTIFIED BY 'Shabab_Ali';
 CREATE USER 'Samuel_Cheng'@'%' IDENTIFIED BY 'Samuel_Cheng';
 
+GRANT ALL PRIVILEGES ON bank_database.* TO 'Adam_Zheng'@'%';
 GRANT ALL PRIVILEGES ON bank_database.* TO 'Hoksolinvan_Chhun'@'%';
 GRANT ALL PRIVILEGES ON bank_database.* TO 'Shabab_Ali'@'%';
 GRANT ALL PRIVILEGES ON bank_database.* TO 'Samuel_Cheng'@'%';
