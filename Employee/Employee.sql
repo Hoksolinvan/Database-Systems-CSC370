9a641a1f7c8c513d045fb1861c7e35e77223b580
Adam Zheng <58533225+AdamZheng2004@users.noreply.github.com>
2024-05-29 15:32:38 -0700

    Add files via upload

--- Employee/Employee.sql
+++ Employee/Employee.sql
@@ -10,8 +10,10 @@ CREATE TABLE `Employee` (
 	,`Title` enum('Branch Manager', 'Assistant Branch Manager', 'Teller', 'Personal Banker', 'Loan Officer', 'Customer Service Representative', 'Financial Advisor')
 	,`Is_Admin` bool
 );
+ALTER TABLE `Employee`
+ADD PRIMARY KEY (`Employee_ID`);
 
-# Load Data Into Table
+# Load Data Into Table (Might need to change path)
 SET GLOBAL local_infile = 1;
 LOAD DATA LOCAL INFILE 'C:\\Users\\adamz\\OneDrive\\Documents\\UVic\\CSC 370\\Database Project\\Employee\\Employee_Data.csv'
 INTO TABLE Employee 
