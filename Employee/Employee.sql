66861be004afcd016fb6113d5451b09122b989a7
Adam Zheng <58533225+AdamZheng2004@users.noreply.github.com>
2024-05-28 19:07:14 -0700

    Add files via upload

--- Employee/Employee.sql
+++ Employee/Employee.sql
@@ -1,19 +1,24 @@
+# Adam Zheng
+
+# Create Table
 USE bank_database;
-
 DROP TABLE IF EXISTS `Employee`;
-
 CREATE TABLE `Employee` (
-	 `Employee_ID` int
-     ,`Age` int
-     ,`Name` varchar(64)
-     ,`Position` enum('Branch Manager', 'Assistant Branch Manager', 'Teller', 'Personal Banker', 'Loan Officer', 'Customer Service Representative', 'Financial Advisor')
+	`Employee_ID` int
+	,`Age` int
+	,`Name` varchar(64)
+	,`Title` enum('Branch Manager', 'Assistant Branch Manager', 'Teller', 'Personal Banker', 'Loan Officer', 'Customer Service Representative', 'Financial Advisor')
+	,`Is_Admin` bool
 );
 
+# Load Data Into Table
 SET GLOBAL local_infile = 1;
-
-LOAD DATA LOCAL INFILE 'Employee_Data.csv'
+LOAD DATA LOCAL INFILE 'C:\\Users\\adamz\\OneDrive\\Documents\\UVic\\CSC 370\\Database Project\\Employee\\Employee_Data.csv'
 INTO TABLE Employee 
 FIELDS TERMINATED BY ',' 
 OPTIONALLY ENCLOSED BY '"' 
 LINES TERMINATED BY '\r\n'
 IGNORE 1 LINES;
+
+# Queries
+SELECT * FROM `Employee`;
