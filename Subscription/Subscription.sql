4e40831729c790f2de58bb7cb2c471036e57ae82
Shabab <22752622+shababmoali@users.noreply.github.com>
2024-06-01 10:21:14 -0700

    adds subscription table and play queries

--- Subscription/Subscription.sql
+++ Subscription/Subscription.sql
@@ -1,7 +1,13 @@
 
 #Table Initialization
-create table `Subscriptions`(`ClientID` INT, `SubscriptionID` varchar(10) unique, 
-`ServiceType` varchar(150), `SubscriptionDate` date, `SubScriptionStatus` enum('Active', 'Inactive', 'Cancelled', 'Pending'), `MonthlyFee` varchar(8) default '$0.00'); 
+drop table if exists Subscription;
+create table Subscriptions(
+    `subscription_id` INT PRIMARY KEY,
+    `client_id` ,  VARCHAR(10) unique,
+    `ServiceType` varchar(150), `SubscriptionDate` date,
+    `SubScriptionStatus` enum('Active', 'Inactive', 'Cancelled', 'Pending'),
+    `MonthlyFee` varchar(8) default '$0.00'
+);
 
 load data local infile '/Users/iamvan/Desktop/CSC370/Clients/Subscriptions/client_subscription.csv' into table `Subscriptions` fields terminated by ',' enclosed by '"' lines terminated by '\r\n' ignore 1 lines;
 
