58ce9da4f07585ed979bfc2589676a52372975ef
Hoksolinvan <chhunhoksolinvan.prof@gmail.com>
2024-06-01 13:45:27 -0700

    standardized and added "JOIN" feature to table
    added "Join" statements to sql code, enabling further complex queries.

--- Client/Client.sql
+++ Client/Client.sql
@@ -5,7 +5,7 @@ create table `Client`(`client_id` int primary key, `account_number` int, `client
 
 #Loading file
 SET GLOBAL local_infile = 1;
-load data local infile 'Client_Data.csv' into table `Client` fields terminated by ',' enclosed by '"' lines terminated by '\r\n' ignore 1 lines;
+load data local infile '/Users/iamvan/Desktop/CSC370/Client/Client_Data.csv' into table `Client` fields terminated by ',' enclosed by '"' lines terminated by '\r\n' ignore 1 lines;
 
 
 #displaying table
@@ -27,6 +27,23 @@ set client_name='poopy'
 where client_id='1';
 
 select * from `Client` where `client_id`=1;
+
+
+create table `Clientbalance`(`client_name` varchar(40), `balance` varchar(40) default '$0');
+load data local infile '/Users/iamvan/Desktop/CSC370/Client/Client_Data.csv' into table `Clientbalance` fields terminated by ',' enclosed by '"' lines terminated by '\r\n' ignore 1 lines (@dummy1, @dummy2, client_name,balance, @dummy3, @dummy4);
+
+select * from `Clientbalance`;
+
+create table `client_account_type`(`client_name` varchar(40), `account_type` enum('None','Unleaded','Premium'));
+load data local infile '/Users/iamvan/Desktop/CSC370/Client/Client_Data.csv' into table `client_account_type` fields terminated by ',' enclosed by '"' lines terminated by '\r\n' ignore 1 lines (@dummy1, @dummy2, client_name,@dummy3, account_type, @dummy4);
+
+select * from `client_account_type`;
+
+select *
+from Clientbalance join client_account_type on `Clientbalance`.client_name= `client_account_type`.client_name;
+
+select *
+from `Clientbalance` left join `client_account_type` on `Clientbalance`.`client_name` = `client_account_type`.`client_name`;
 
 
 
