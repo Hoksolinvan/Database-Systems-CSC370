fd0116e3ed48f87ff286765a9ff1ba819934669f
Adam Zheng <58533225+AdamZheng2004@users.noreply.github.com>
2024-06-05 16:05:30 -0700

    Updated all Bank files

--- Bank/Bank.sql
+++ Bank/Bank.sql
@@ -1,6 +1,7 @@
 # Adam Zheng
 
 # Create Table
+use bank_database;
 drop table if exists `Bank`;
 create table `Bank` (
 	`branch_num` int
@@ -11,6 +12,8 @@ create table `Bank` (
 alter table `Bank`
 add primary key (`branch_num`);
 
+
+
 # Load Data Into Table (Might need to change path)
 set global local_infile = 1;
 load data local INFILE 'C:\\Users\\adamz\\OneDrive\\Documents\\UVic\\CSC 370\\Database Project\\Bank\\Bank_Data.csv'
@@ -20,8 +23,20 @@ optionally enclosed by '"'
 lines terminated by '\r\n'
 ignore 1 lines;
 
+
 # Queries
 select * from `Bank`;
 
-# Update num_employee and num_client (TODO Sprint 2)
-# add a time hired and fired table that is join of Bank and Employee
+# Update num_employee with subquery 
+#(will need to comment out all the create table and load data lines before uncommenting and running this)
+/*
+set SQL_SAFE_UPDATES = 0;
+update `Bank` 
+set `num_employees` = (
+	select count(*)
+    from `Employee`
+    where `branch_num` = 1005151
+    );
+set SQL_SAFE_UPDATES = 1;
+select * from `Bank`;
+*/
